33e2c0a81072bb69c57e764c885fa9ab665394f0
Charlie LECHAUVE <lechauve.charlie@orange.fr>
2024-06-01 17:19:31 +0200

    test git pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    basePath: '/nom-de-votre-repo', // Remplacez par le nom de votre dépôt GitHub
+    assetPrefix: '/nom-de-votre-repo/', // Remplacez par le nom de votre dépôt GitHub
+    trailingSlash: true, // Ajoute un slash à la fin des chemins
+    output: 'export' // Ajoutez cette ligne pour configurer l'exportation statique
+  }
+  
+  module.exports = nextConfig;
+  
